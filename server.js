e696e4c673804686cd6299b2c212d27e7730d707
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-06-14 17:59:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
     console.log(`listen on http://localhost:${PORT}`)
