2398686a29e893fdb6dc745cd39e070ee5bde7fc
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-06-14 18:00:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4444;
 
